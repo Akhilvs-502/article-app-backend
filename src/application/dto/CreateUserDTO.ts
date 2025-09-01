22a73fd476fb5f1e888e60966837ca2ee31fecbc
akhilvskannan77@gmail.com <akhilvskannan77@gmail.com>
2025-09-02 01:48:20 +0530

    preference added in ccreateDto

--- src/application/dto/CreateUserDTO.ts
+++ src/application/dto/CreateUserDTO.ts
@@ -12,11 +12,14 @@ export class CreateUserDTO {
 
   dateOfBirth:string
 
-  constructor(data:{firstName:string, lastName: string; email: string; password: string;dateOfBirth:string }) {
+  preferences:string[]
+
+  constructor(data:{firstName:string, lastName: string; email: string; password: string;dateOfBirth:string,preferences:string[]}) {
     this.firstName = data.firstName.trim();
     this.lastName = data.lastName.trim();
     this.email = data.email.trim().toLowerCase();
     this.password = data.password;
     this.dateOfBirth=data.dateOfBirth
+    this.preferences=data.preferences
   }
 }
